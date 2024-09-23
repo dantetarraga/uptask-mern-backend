12c44097f894d2d651942d7f5cdcdd73955b3f55
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-23 13:24:12 -0500

    Agregar validación de campos para la creación de proyectos

--- src/routes/project.router.ts
+++ src/routes/project.router.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
 import { ProjectController } from '../controllers/project.controller'
+import { projectValidator } from '../validators/project.validator'
+import { validatorFields } from '../middlewares/validator.fields'
 
 const projectRouter = Router()
 
-projectRouter.get('/', ProjectController.getAllProjects)
+projectRouter.get(
+  '/',
+  [...projectValidator],
+  validatorFields,
+  ProjectController.getAllProjects
+)
 projectRouter.post('/', ProjectController.createProject)
 
 export default projectRouter
